1204f882c7c8c55e6415a1d9f55b3233a653884e
André Forner <andrefornerr@gmail.com>
2024-06-18 20:12:32 -0300

    Commit da aula 3

---
+++ 1 - Java e Refatoração, melhorando códigos com boas práticas/src/main/java/br/com/alura/domain/Abrigo.java
@@ -0,0 +1,22 @@
+package br.com.alura.domain;
+
+public class Abrigo {
+    private long id;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Abrigo(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
