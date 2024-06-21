fb4814a3db5338086a87cb166e88b23c9fca962a
Ivan Forner <ivanforner@gmail.com>
2024-06-21 08:01:06 -0300

    Aprendendo java em casa

--- 3 - Crie Aplicações Usando Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ 3 - Crie Aplicações Usando Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,24 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* Para que essa aplicação rode em linha de comando, o método principal precisa implementar
+a classe CommandLineRunner, como realizado abaixo
+*/
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9613a928");
+		System.out.println(json);
+	}
 }
--- 3 - Crie Aplicações Usando Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ 3 - Crie Aplicações Usando Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,2 +1,10 @@
-package br.com.alura.screenmatch.model;public record DadosSerie() {
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+// A anotação JsonAlias lê a chave json e escreve a chave que você define
+// A anotação JsonProperty lê e escreve a propriedade da maneira como ela vem da API.
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") int totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
